5fac324d3afba36aa869246f87e9aa61f03c8b67
Jeff Wong <v-jwong1@expedia.com>
2020-04-19 16:18:41 +1000

    create setter for speaker repository in SpeakerServiceImpl

--- src/main/java/com/pluralsight/service/SpeakerServiceImpl.java
+++ src/main/java/com/pluralsight/service/SpeakerServiceImpl.java
@@ -1,13 +1,16 @@
 package com.pluralsight.service;
 
 import com.pluralsight.model.Speaker;
-import com.pluralsight.repository.HibernateSpeakerRepositoryImpl;
 import com.pluralsight.repository.SpeakerRepository;
 
 import java.util.List;
 
 public class SpeakerServiceImpl implements SpeakerService {
-    private final SpeakerRepository repository = new HibernateSpeakerRepositoryImpl();
+    private SpeakerRepository repository;
+
+    public void setRepository(SpeakerRepository repository) {
+        this.repository = repository;
+    }
 
     public List<Speaker> findAll() {
         return repository.findAll();
