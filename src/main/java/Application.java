8d66397894cc4bb422208504046f0f6ac9325a37
Jeff Wong <v-jwong1@expedia.com>
2020-04-19 16:37:41 +1000

    create application context in main Application class

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,8 +1,12 @@
 import com.pluralsight.service.SpeakerService;
 import com.pluralsight.service.SpeakerServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Application {
     public static void main (String[] args) {
+        final ApplicationContext appContext = new AnnotationConfigApplicationContext(AppConfig.class);
+
         final SpeakerService service = new SpeakerServiceImpl();
         System.out.println(service.findAll().get(0).getFirstName());
     }
