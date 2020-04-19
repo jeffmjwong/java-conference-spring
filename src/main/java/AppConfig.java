5e97331e43b17962c195c3bde3c3d35bbadb3914
Jeff Wong <v-jwong1@expedia.com>
2020-04-19 15:34:27 +1000

    create getSpeakerService method in AppConfig

--- src/main/java/AppConfig.java
+++ src/main/java/AppConfig.java
@@ -1,6 +1,10 @@
 import com.pluralsight.service.SpeakerService;
+import com.pluralsight.service.SpeakerServiceImpl;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AppConfig {
+    public SpeakerService getSpeakerService() {
+        return new SpeakerServiceImpl();
+    }
 }
