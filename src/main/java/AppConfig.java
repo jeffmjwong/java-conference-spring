347a909de6fd70df1d2390e2c4c3bb3ba6fdeee2
Jeff Wong <v-jwong1@expedia.com>
2020-04-20 16:10:27 +1000

    add @ComponentScan annotation to AppConfig

--- src/main/java/AppConfig.java
+++ src/main/java/AppConfig.java
@@ -4,10 +4,12 @@ import com.pluralsight.service.SpeakerService;
 import com.pluralsight.service.SpeakerServiceImpl;
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
 
 @Configuration
+@ComponentScan({"com.pluralsight"})
 public class AppConfig {
     @Bean(name = "speakerService")
     @Scope(BeanDefinition.SCOPE_SINGLETON)
