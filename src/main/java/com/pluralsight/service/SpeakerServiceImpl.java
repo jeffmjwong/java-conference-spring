aeab25ff2631b1c744b8229e0987ed7326fd7a6b
Jeff Wong <v-jwong1@expedia.com>
2020-04-20 16:13:12 +1000

    annotate SpeakerServiceImpl class with @Service

--- src/main/java/com/pluralsight/service/SpeakerServiceImpl.java
+++ src/main/java/com/pluralsight/service/SpeakerServiceImpl.java
@@ -3,9 +3,11 @@ package com.pluralsight.service;
 import com.pluralsight.model.Speaker;
 import com.pluralsight.repository.SpeakerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service("speakerService")
 public class SpeakerServiceImpl implements SpeakerService {
     private SpeakerRepository repository;
 
