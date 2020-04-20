8f4bb43c612d94c5dfdf133238d49e13f0508109
Jeff Wong <v-jwong1@expedia.com>
2020-04-20 15:16:12 +1000

    test whether speakerService is really a singleton

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -8,7 +8,11 @@ public class Application {
         final ApplicationContext appContext = new AnnotationConfigApplicationContext(AppConfig.class);
 
         final SpeakerService service = appContext.getBean("speakerService", SpeakerService.class);
+        System.out.println(service);
 
         System.out.println(service.findAll().get(0).getFirstName());
+
+        final SpeakerService service2 = appContext.getBean("speakerService", SpeakerService.class);
+        System.out.println(service2);
     }
 }
