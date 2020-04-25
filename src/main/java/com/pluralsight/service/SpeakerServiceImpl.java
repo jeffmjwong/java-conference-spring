0aa4beabc4e53be80fa8859e3ddd7a2c5907f682
Jeff Wong <v-jwong1@expedia.com>
2020-04-25 17:44:54 +1000

    use @PostConstruct annotation

--- src/main/java/com/pluralsight/service/SpeakerServiceImpl.java
+++ src/main/java/com/pluralsight/service/SpeakerServiceImpl.java
@@ -5,6 +5,7 @@ import com.pluralsight.repository.SpeakerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.PostConstruct;
 import java.util.List;
 
 @Service("speakerService")
@@ -19,6 +20,11 @@ public class SpeakerServiceImpl implements SpeakerService {
     public SpeakerServiceImpl(SpeakerRepository repository) {
         System.out.println("SpeakerServiceImpl repository constructor!");
         this.repository = repository;
+    }
+
+    @PostConstruct
+    private void initialize() {
+        System.out.println("We're called after the constructors!");
     }
 
 //    @Autowired
