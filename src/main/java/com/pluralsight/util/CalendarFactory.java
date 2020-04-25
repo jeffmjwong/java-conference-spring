89952b0e65f0d9873ca300478565ed9fcaaa25af
Jeff Wong <v-jwong1@expedia.com>
2020-04-25 20:37:19 +1000

    getObjectType

--- src/main/java/com/pluralsight/util/CalendarFactory.java
+++ src/main/java/com/pluralsight/util/CalendarFactory.java
@@ -14,6 +14,6 @@ public class CalendarFactory implements FactoryBean<Calendar> {
 
     @Override
     public Class<?> getObjectType() {
-        return null;
+        return Calendar.class;
     }
 }
