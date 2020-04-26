d669a4de333d0892e16d1f4d967c8c2abf40028e
Jeff Wong <v-jwong1@expedia.com>
2020-04-26 10:19:39 +1000

    create cal bean that returns a CalendarFactory instance

--- src/main/java/AppConfig.java
+++ src/main/java/AppConfig.java
@@ -2,6 +2,7 @@ import com.pluralsight.repository.HibernateSpeakerRepositoryImpl;
 import com.pluralsight.repository.SpeakerRepository;
 import com.pluralsight.service.SpeakerService;
 import com.pluralsight.service.SpeakerServiceImpl;
+import com.pluralsight.util.CalendarFactory;
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -11,6 +12,13 @@ import org.springframework.context.annotation.Scope;
 @Configuration
 @ComponentScan({"com.pluralsight"})
 public class AppConfig {
+    @Bean(name = "cal")
+    public CalendarFactory calFactory() {
+        final CalendarFactory factory = new CalendarFactory();
+        factory.addDays(2);
+        return factory;
+    }
+
 //    @Bean(name = "speakerService")
 //    @Scope(BeanDefinition.SCOPE_SINGLETON)
 //    public SpeakerService getSpeakerService() {
