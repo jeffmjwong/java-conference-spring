f1b67efede1ceb5c78c23088593391dd85846024
Jeff Wong <v-jwong1@expedia.com>
2020-04-26 10:53:34 +1000

    create @Value for a seed number using Spring

--- src/main/java/com/pluralsight/repository/HibernateSpeakerRepositoryImpl.java
+++ src/main/java/com/pluralsight/repository/HibernateSpeakerRepositoryImpl.java
@@ -2,6 +2,7 @@ package com.pluralsight.repository;
 
 import com.pluralsight.model.Speaker;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
@@ -13,14 +14,19 @@ public class HibernateSpeakerRepositoryImpl implements SpeakerRepository {
     @Autowired
     private Calendar calendar;
 
+    @Value("#{ T(java.lang.Math).random() * 100 }")
+    private double seedNumber;
+
     public List<Speaker> findAll() {
         final List<Speaker> speakers = new ArrayList<>();
 
         final Speaker speaker = new Speaker();
         speaker.setFirstName("Bryan");
         speaker.setLastName("Hansen");
+        speaker.setSeedNumber(seedNumber);
 
         System.out.println("calendar: " + calendar.getTime());
+        System.out.println(seedNumber);
 
         speakers.add(speaker);
 
