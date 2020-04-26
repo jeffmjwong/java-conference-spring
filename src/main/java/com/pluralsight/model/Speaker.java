8798a29fc9536ae2979c14a9f112aa1fee31c5c5
Jeff Wong <v-jwong1@expedia.com>
2020-04-26 10:50:02 +1000

    create double field seedNumber

--- src/main/java/com/pluralsight/model/Speaker.java
+++ src/main/java/com/pluralsight/model/Speaker.java
@@ -3,6 +3,7 @@ package com.pluralsight.model;
 public class Speaker {
     private String firstName;
     private String lastName;
+    private double seedNumber;
 
     public String getFirstName() {
         return firstName;
