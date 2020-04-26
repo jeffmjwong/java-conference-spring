165d073daaebdd761b90ed136ac7839b61e1e3db
Jeff Wong <v-jwong1@expedia.com>
2020-04-26 10:56:23 +1000

    move seed number print ln to main application

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -9,5 +9,6 @@ public class Application {
         final SpeakerService service = appContext.getBean("speakerService", SpeakerService.class);
 
         System.out.println(service.findAll().get(0).getFirstName());
+        System.out.println(service.findAll().get(0).getSeedNumber());
     }
 }
