c2f505f0b91cc587f6286620b569b5c37432b451
Jeff Wong <v-jwong1@expedia.com>
2020-04-26 10:23:18 +1000

    create @Autowired private calendar instance in HibernateSpeakerRepositoryImpl

--- src/main/java/com/pluralsight/repository/HibernateSpeakerRepositoryImpl.java
+++ src/main/java/com/pluralsight/repository/HibernateSpeakerRepositoryImpl.java
@@ -1,13 +1,18 @@
 package com.pluralsight.repository;
 
 import com.pluralsight.model.Speaker;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 @Repository("speakerRepository")
 public class HibernateSpeakerRepositoryImpl implements SpeakerRepository {
+    @Autowired
+    private Calendar calendar;
+
     public List<Speaker> findAll() {
         final List<Speaker> speakers = new ArrayList<>();
 
